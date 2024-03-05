641340f664e03295f13ee9d12740996c7bf3ddb5
unknown <you@example.com>
2024-03-05 23:16:50 +0300

    first commit

---
+++ datatypes.py
@@ -0,0 +1,44 @@
+# Data types
+
+c= 20.345
+d= 10+3j
+print("The type of variable having value c", c, "is", type(c))
+print("print type of variable having value d", d, "is", type(d))
+
+
+# String data sequence
+
+str = "Hello world"
+print(str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str + ", Tony here")
+print(str * 5 )
+
+# list_data sequence
+
+list = [2024, "Python", 3.14, 5+6j, 1.23E-4]
+print(list)
+
+
+list2 = ["python", 3.14]
+print(list2)
+print(list + list2)
+print(list * 4)
+
+# Tuple   Data types
+
+tuple = 'abcd', 251, 3.145, 'Tony', 1.23e-2
+print(tuple)
+print(tuple[2:4])
+
+
+# Dictionary Data types
+
+dictionary = {1:'one', 2:'two', 3:'three'}
+
+print(dictionary)
+dict[1] = 'This is one'
+dict[2] = 'This is two'
+print(dict1)
